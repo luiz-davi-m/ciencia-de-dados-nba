2c55c02b4458218ddb06fa9c9b21e5498b4f835a
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2025-01-18 09:24:49 -0300

    feature[parte 1]: finalizado RF7

--- Parte 1/constantes.py
+++ Parte 1/constantes.py
@@ -1,4 +1,8 @@
+from nba_api_utils import obter_time
+
 NOME_TIME = 'Denver Nuggets'
+time = obter_time(NOME_TIME)
+TIME_ID = time['id']
 
 TEMPORADA_ANTERIOR = '2023-24'
 TEMPORADA_ATUAL = '2024-25'
