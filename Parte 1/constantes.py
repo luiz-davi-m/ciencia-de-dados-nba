2084c8b02965197b1d46d3126ade6d544f60db31
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2025-01-18 00:47:41 -0300

    feature[parte 1]: adicionado arquivo de constantes

---
+++ Parte 1/constantes.py
@@ -0,0 +1,8 @@
+NOME_TIME = 'Denver Nuggets'
+
+TEMPORADA_ANTERIOR = '2023-24'
+TEMPORADA_ATUAL = '2024-25'
+
+JOGADOR_1 = 'Nikola Jokic'
+JOGADOR_2 = 'Michael Porter Jr'
+JOGADOR_3 = 'Jamal Murray'
