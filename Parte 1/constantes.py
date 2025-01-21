5c940e1b907b5412f0ebc557f97ba66a831504b2
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2025-01-21 02:18:36 -0300

    refac[parte 1]: alterado nome de arquivo e removidas constantes não utilizadas

--- Parte 1/constantes.py
+++ Parte 1/constantes.py
@@ -7,8 +7,4 @@ TIME_ID = time['id']
 TEMPORADA_ANTERIOR = '2023-24'
 TEMPORADA_ATUAL = '2024-25'
 
-JOGADOR_1 = 'Nikola Jokic'
-JOGADOR_2 = 'Michael Porter Jr'
-JOGADOR_3 = 'Jamal Murray'
-
 RESULTADOS_PATH = '../resultados'
